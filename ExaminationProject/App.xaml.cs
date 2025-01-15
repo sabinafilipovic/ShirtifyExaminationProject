b5a643fcb08b01201c3860b248a7d661b99d1738
Lukas Graff <Lukas.Graff@brightstraining.com>
2025-01-15 09:47:37 +0100

    Add project files.

---
+++ ExaminationProject/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ExaminationProject
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ExaminationProject/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ExaminationProject
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
